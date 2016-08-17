1f2d945024b32ed34c34ac62fa98fc0751fba9fe
luccasmf <luccas_mf@live.com>
2016-08-16 21:29:10 -0300

    Reenvio de arquivos

---
+++ ControleDocumentosLibrary/EnumStatusEvento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDocumentosLibrary
+{
+    public enum EnumStatusEvento : int
+    {
+        aprovado =1,
+        cancelado,
+        adiado,
+        concluido,
+
+    }
+}
---
+++ ControleDocumentosLibrary/EnumStatusSolicitacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDocumentosLibrary
+{
+    public enum EnumStatusSolicitacao : int
+    {
+        pendente =1,
+        visualizado,
+        processando,
+        concluido,
+        cancelado,
+    }
+}
