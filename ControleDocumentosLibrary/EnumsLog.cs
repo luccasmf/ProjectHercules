62a98809be8e62ed9a6b77e15f75677aabf5b138
luccasmf <luccas_mf@live.com>
2016-10-25 23:24:41 -0200

    adicionado repositorio de Logs e arquivo DOC do tcc ao projeto

--- ControleDocumentosLibrary/EnumsLog.cs
+++ ControleDocumentosLibrary/EnumsLog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,20 +9,32 @@ namespace ControleDocumentosLibrary
 {
     public enum EnumAcao : int
     {
-        Criar =1,
+
+        [Description("Criar")]
+        Criar = 1,
+        [Description("Editar")]
         Editar,
+        [Description("Ativar")]
         Ativar,
+        [Description("Desativar")]
         Desativar,
+        [Description("Excluir")]
         Excluir,
     }
 
     public enum EnumTipoObjeto : int
     {
-        Aluno=1,
+        [Description("Aluno")]
+        Aluno = 1,
+        [Description("Curso")]
         Curso,
+        [Description("Documento")]
         Documento,
+        [Description("Evento")]
         Evento,
+        [Description("Funcionario")]
         Funcionario,
+        [Description("Usuario")]
         Usuario
     }
 }
