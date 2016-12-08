dcd02df412cf886f4628febd14aeb7c1b100bf0f
luccasmf <luccas_mf@live.com>
2016-12-08 13:11:30 -0200

    commit com ajustes para publicação

--- ControleDocumentos/Controllers/LogController.cs
+++ ControleDocumentos/Controllers/LogController.cs
@@ -10,7 +10,7 @@ using System.Web.Mvc;
 
 namespace ControleDocumentos.Controllers
 {
-    [AuthorizeAD]
+    //[AuthorizeAD]
     public class LogController : Controller
     {
         LogsRepository logRepository = new LogsRepository();
